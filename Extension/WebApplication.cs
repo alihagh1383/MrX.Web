c6ec1ef337909a3f0d024858fd7dbb4adb982233
Ali Hagh <alihagh1383@gmail.com>
2024-10-31 09:02:28 +0330

    add db workers

--- Extension/WebApplication.cs
+++ Extension/WebApplication.cs
@@ -1,7 +1,9 @@
-﻿namespace MrX.Extensions;
+﻿using Microsoft.AspNetCore.Builder;
 
-public static class WebApplication
+namespace MrX.Web.Extension;
+
+public static class WebApplicationExtension
 {
-    public static void UseMiddlewareForPath<T>(this WebApplication app, string path) => app.UseWhen(configuration: appBuilder => appBuilder.UseMiddleware<T>(), predicate: context => context.Request.Path.StartsWithSegments(path));
+    public static void UseMiddlewareForPath<T>(this Microsoft.AspNetCore.Builder.IApplicationBuilder app, string path) => app.UseWhen(configuration: appBuilder => appBuilder.UseMiddleware<T>(), predicate: context => context.Request.Path.StartsWithSegments(path));
 
 }
