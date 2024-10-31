f283c012ee715cc7b6ea71e4aa96952b1890ae84
Ali Hagh <alihagh1383@gmail.com>
2024-10-31 09:01:43 +0330

    add DB Workers

---
+++ Extension/WebApplication.cs
@@ -0,0 +1,7 @@
+﻿namespace MrX.Extensions;
+
+public static class WebApplication
+{
+    public static void UseMiddlewareForPath<T>(this WebApplication app, string path) => app.UseWhen(configuration: appBuilder => appBuilder.UseMiddleware<T>(), predicate: context => context.Request.Path.StartsWithSegments(path));
+
+}
