3cdaced855ceeef9b62e7a120376b046be33c7ce
X Sean <alihagh1383@gmail.com>
2025-02-20 15:17:47 +0330

    Add Middleware

---
+++ Middleware/ApiExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MrX.Web.ApiCallback;
+using MrX.Web.Exceptions;
+using Newtonsoft.Json;
+
+namespace MrX.Web.Middleware
+{
+    public class ApiExceptionHandler(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ApiException aex)
+                {
+                    httpContext.Response.StatusCode = aex.StatusCode;
+                    httpContext.Response.WriteAsync(aex.ToReturn().ToString(),Encoding.UTF8).Wait();
+                }
+                else { throw; }
+            }
+        }
+    }
+}
