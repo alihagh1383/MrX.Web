530c1b43936327cc64c43df556f5596c1b14ddf0
MrX <alihagh1383@gmail.com>
2025-03-21 16:21:55 +0330

    Add Hash

---
+++ Security/PasswordHash.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+
+namespace MrX.Web.Security;
+
+public static class PasswordHash
+{
+    private const char SegmentDelimiter = ':';
+    private static readonly int SaltSize = System.Random.Shared.Next(20, 100); // 128 bits
+    private static readonly int KeySize = System.Random.Shared.Next(20, 100); // 256 bits
+    private static readonly int Iterations = System.Random.Shared.Next(20, 100);
+    private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA256;
+
+    public static string Hash(string input)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            input,
+            salt,
+            Iterations,
+            Algorithm,
+            KeySize
+        );
+        return string.Join(
+            SegmentDelimiter,
+            Convert.ToHexString(hash),
+            Convert.ToHexString(salt),
+            Iterations,
+            Algorithm
+        );
+    }
+
+    public static bool Verify(string input, string hashString)
+    {
+        string[] segments = hashString.Split(SegmentDelimiter);
+        var hash = Convert.FromHexString(segments[0]);
+        var salt = Convert.FromHexString(segments[1]);
+        var iterations = int.Parse(segments[2]);
+        var algorithm = new HashAlgorithmName(segments[3]);
+        var inputHash = Rfc2898DeriveBytes.Pbkdf2(
+            input,
+            salt,
+            iterations,
+            algorithm,
+            hash.Length
+        );
+        return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+    }
+}
