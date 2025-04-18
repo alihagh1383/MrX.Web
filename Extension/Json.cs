a9915b7c85c4e513d6fac67d633f825767153803
MrX <alihagh1383@gmail.com>
2025-04-18 15:13:07 +0330

    [Update(Json.cs)][Imp(FromJson<T>)][Imp(FromJson)][Imp(ToJObject)]

--- Extension/Json.cs
+++ Extension/Json.cs
@@ -1,6 +1,11 @@
+using Newtonsoft.Json.Linq;
+
 namespace MrX.Web.Extension;
 
 public static class Json
 {
     public static string ToJson(this object obj) => Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+    public static T? FromJson<T>(this string json) => Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+    public static object? FromJson(this string json) => Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+    public static JObject ToJObject(this string json) => Newtonsoft.Json.Linq.JObject.Parse(json);
 }
