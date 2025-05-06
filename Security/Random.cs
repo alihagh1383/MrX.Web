918f139c9a4eb230e6ffd4bf799280894a4227bc
MrX <Alihagh1383@gmail.com>
2025-05-06 21:34:17 +0330

    [Improve] Random String

--- Security/Random.cs
+++ Security/Random.cs
@@ -16,20 +16,23 @@ public static class Random
         bool lowerCase = false,
         bool uppercase = false,
         bool number = true,
-        int? seed = null
+        int? seed = null,
+        bool whiteSpase = false,
+        char[]? lowerCaseSet = null
     )
     {
         var list = new List<char>();
-        if (lowerCase)
-            for (var c = 'a'; c <= 'z'; ++c)
-                list.Add(c);
         if (uppercase)
-            for (var c = 'A'; c <= 'Z'; ++c)
-                list.Add(c);
+            for (var c = 'A'; c <= 'Z'; ++c) list.Add(c);
         if (number)
-            for (short c = 0; c <= 9; ++c)
-                list.Add(c.ToString()[0]);
-
+            for (short c = 0; c <= 9; ++c) list.Add(c.ToString()[0]);
+        if (whiteSpase)
+            list.Add(' ');
+        if (lowerCase)
+            if (lowerCaseSet is null)
+                for (var c = 'a'; c <= 'z'; ++c) list.Add(c);
+            else if (lowerCaseSet is { })
+                list.AddRange(lowerCaseSet);
         var o = ((seed != null) ? new System.Random((int)seed!) : System.Random.Shared).GetItems(list.ToArray(), len);
         return string.Join("", o);
     }
